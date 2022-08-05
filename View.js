15dd0074e7f71cc3eb4b9df138551a106c6d40fc
shivani-droid <84192940+shivani-droid@users.noreply.github.com>
2022-08-05 12:22:11 +0530

    Add files via upload

---
+++ View.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export const View = ({books,deleteBook}) => {
+    
+    return books.map(book=>(
+        
+        <tr key={book.isbn}>
+            <td>{book.isbn}</td>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            <td className='delete-btn' onClick={()=>deleteBook(book.isbn)}>
+               
+            </td>           
+        </tr>            
+    
+))
+}
